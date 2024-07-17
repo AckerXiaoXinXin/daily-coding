acc9516acaf779fac5331512ad97e240b20f8183
lixin09 <lixin09@zhihu.com>
2024-07-17 10:07:06 +0800

    t

---
+++ Open_source_LLM/implement_test.py
@@ -0,0 +1,24 @@
+"""
+Project Name: LLM_LEARNING
+Module Description: test using open source LLM
+Author: lixin
+Date: 2024-07-15
+Version: 1.0.0
+Email: 395095201@qq.com, zangaixiaoxinxin@gmail.com
+
+Description:
+    None.
+
+Usage:
+    None.
+
+Examples:
+    If applicable, include a simple usage example.
+
+Todo:
+    - 
+
+@file: LLM_LEARNING/Open_source_LLM
+@software: Visual Studio Code
+"""
+
---
+++ Transformer_Learning/transformer_reimplement_full_code.py
@@ -0,0 +1,25 @@
+"""
+Project Name: LLM_LEARNING
+Module Description: Reimplement transfromer with full code
+Author: lixin
+Date: 2024-07-15
+Version: 1.0.0
+Email: 395095201@qq.com, zangaixiaoxinxin@gmail.com
+
+Description:
+    None.
+
+Usage:
+    None.
+
+Examples:
+    If applicable, include a simple usage example.
+
+Todo:
+    - Reimplement transfromer with full code.
+
+@file: LLM_LEARNING/Transformer_Learning
+@software: Visual Studio Code
+"""
+
+
